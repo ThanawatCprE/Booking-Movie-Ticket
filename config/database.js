7777933f7c75c5ae20cd4cbdc682365e4ed7ed13
Thanawat Kahothong <thanawat0688@gmail.com>
2017-05-02 19:56:02 +0700

    change login function
    change login compare with database to use passport module and add
    function register of customer

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': '20022539'
+
+        
+    },
+	'database': 'movieticket',
+    'users_table': 'customer'
+};
